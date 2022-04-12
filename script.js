6aca18ddfdba6b759879019531092fc1e57270e9
shtag <vasyakanishchev@gmail.com>
2022-04-12 14:11:19 +0300

    red button

---
+++ script.js
@@ -0,0 +1,44 @@
+let contacts = [
+  {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+
+
+function lookUpProfile(name, prop){
+    let contact = contacts.find(item => item.firstname == name);
+    
+    if (contact) {
+      if (contact[prop]) {
+        return contact[prop];
+      } else { 
+        return 'no prop';
+      }
+    } else {
+      return 'no contact';
+  }
+}
+
+
+console.log(lookUpProfile("Kristian", "lastName"));
